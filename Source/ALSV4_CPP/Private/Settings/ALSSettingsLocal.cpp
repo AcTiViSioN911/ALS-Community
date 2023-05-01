25428ae4787ad3e1ae8449453546e38505a25c0e
Helcio Laurentino do Carmo Junior <helcio@carmo.in>
2023-05-01 18:31:00 -0300

    feat(settings): Add setting class

---
+++ Source/ALSV4_CPP/Private/Settings/ALSSettingsLocal.cpp
@@ -0,0 +1,175 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Settings/ALSSettingsLocal.h"
+#include "CommonInputBaseTypes.h"
+#include "CommonInputSubsystem.h"
+#include "PlayerMappableInputConfig.h"
+#include "EnhancedInputSubsystems.h"
+#include "Player/ALSLocalPlayer.h"
+
+UALSSettingsLocal::UALSSettingsLocal()
+{
+}
+
+UALSSettingsLocal* UALSSettingsLocal::Get()
+{
+	// HACK: Fix this
+	return GEngine ? CastChecked<UALSSettingsLocal>(GEngine->GetGameUserSettings()) : nullptr;
+}
+
+void UALSSettingsLocal::BeginDestroy()
+{
+	Super::BeginDestroy();
+}
+
+void UALSSettingsLocal::SetControllerPlatform(const FName InControllerPlatform)
+{
+	if (ControllerPlatform != InControllerPlatform)
+	{
+		ControllerPlatform = InControllerPlatform;
+
+		// Apply the change to the common input subsystem so that we refresh any input icons we're using.
+		if (UCommonInputSubsystem* InputSubsystem = UCommonInputSubsystem::Get(GetTypedOuter<ULocalPlayer>()))
+		{
+			InputSubsystem->SetGamepadInputType(ControllerPlatform);
+		}
+	}
+}
+
+FName UALSSettingsLocal::GetControllerPlatform() const
+{
+	return ControllerPlatform;
+}
+
+void UALSSettingsLocal::RegisterInputConfig(ECommonInputType Type, const UPlayerMappableInputConfig* NewConfig, const bool bIsActive)
+{
+	if (NewConfig)
+	{
+		const int32 ExistingConfigIdx = RegisteredInputConfigs.IndexOfByPredicate( [&NewConfig](const FLoadedMappableConfigPair& Pair) { return Pair.Config == NewConfig; } );
+		if (ExistingConfigIdx == INDEX_NONE)
+		{
+			const int32 NumAdded = RegisteredInputConfigs.Add(FLoadedMappableConfigPair(NewConfig, Type, bIsActive));
+			if (NumAdded != INDEX_NONE)
+			{
+				OnInputConfigRegistered.Broadcast(RegisteredInputConfigs[NumAdded]);
+			}	
+		}
+	}
+}
+
+int32 UALSSettingsLocal::UnregisterInputConfig(const UPlayerMappableInputConfig* ConfigToRemove)
+{
+	if (ConfigToRemove)
+	{
+		const int32 Index = RegisteredInputConfigs.IndexOfByPredicate( [&ConfigToRemove](const FLoadedMappableConfigPair& Pair) { return Pair.Config == ConfigToRemove; } );
+		if (Index != INDEX_NONE)
+		{
+			RegisteredInputConfigs.RemoveAt(Index);
+			return 1;
+		}
+			
+	}
+	return INDEX_NONE;
+}
+
+void UALSSettingsLocal::ActivateInputConfig(const UPlayerMappableInputConfig* Config)
+{
+	if (Config)
+	{
+		const int32 ExistingConfigIdx = RegisteredInputConfigs.IndexOfByPredicate( [&Config](const FLoadedMappableConfigPair& Pair) { return Pair.Config == Config; } );
+		if (ExistingConfigIdx != INDEX_NONE)
+		{
+			RegisteredInputConfigs[ExistingConfigIdx].bIsActive = true;
+			OnInputConfigActivated.Broadcast(RegisteredInputConfigs[ExistingConfigIdx]);
+		}
+	}
+}
+
+void UALSSettingsLocal::DeactivateInputConfig(const UPlayerMappableInputConfig* Config)
+{
+	if (Config)
+	{
+		const int32 ExistingConfigIdx = RegisteredInputConfigs.IndexOfByPredicate( [&Config](const FLoadedMappableConfigPair& Pair) { return Pair.Config == Config; } );
+		if (ExistingConfigIdx != INDEX_NONE)
+		{
+			RegisteredInputConfigs[ExistingConfigIdx].bIsActive = false;
+			OnInputConfigDeactivated.Broadcast(RegisteredInputConfigs[ExistingConfigIdx]);
+		}
+	}
+}
+
+const UPlayerMappableInputConfig* UALSSettingsLocal::GetInputConfigByName(FName ConfigName) const
+{
+	for (const FLoadedMappableConfigPair& Pair : RegisteredInputConfigs)
+	{
+		if (Pair.Config->GetConfigName() == ConfigName)
+		{
+			return Pair.Config;
+		}
+	}
+	return nullptr;
+}
+
+void UALSSettingsLocal::GetRegisteredInputConfigsOfType(ECommonInputType Type, TArray<FLoadedMappableConfigPair>& OutArray) const
+{
+	OutArray.Empty();
+
+	// If "Count" is passed in then 
+	if (Type == ECommonInputType::Count)
+	{
+		OutArray = RegisteredInputConfigs;
+		return;
+	}
+	
+	for (const FLoadedMappableConfigPair& Pair : RegisteredInputConfigs)
+	{
+		if (Pair.Type == Type)
+		{
+			OutArray.Emplace(Pair);
+		}
+	}
+}
+
+void UALSSettingsLocal::AddOrUpdateCustomKeyboardBindings(const FName MappingName, const FKey NewKey, UALSLocalPlayer* LocalPlayer)
+{
+	if (MappingName == NAME_None)
+	{
+		return;
+	}
+	
+	if (InputConfigName != TEXT("Custom"))
+	{
+		// Copy Presets.
+		if (const UPlayerMappableInputConfig* DefaultConfig = GetInputConfigByName(TEXT("Default")))
+		{
+			for (const FEnhancedActionKeyMapping& Mapping : DefaultConfig->GetPlayerMappableKeys())
+			{
+				// Make sure that the mapping has a valid name, its possible to have an empty name
+				// if someone has marked a mapping as "Player Mappabe" but deleted the default field value
+				if (Mapping.PlayerMappableOptions.Name != NAME_None)
+				{
+					CustomKeyboardConfig.Add(Mapping.PlayerMappableOptions.Name, Mapping.Key);
+				}
+			}
+		}
+		
+		InputConfigName = TEXT("Custom");
+	} 
+
+	if (CustomKeyboardConfig.Find(MappingName))
+	{
+		// Change the key to the new one
+		CustomKeyboardConfig[MappingName] = NewKey;
+	}
+	else
+	{
+		CustomKeyboardConfig.Add(MappingName, NewKey);
+	}
+
+	// Tell the enhanced input subsystem for this local player that we should remap some input! Woo
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer))
+	{
+		Subsystem->AddPlayerMappedKey(MappingName, NewKey);
+	}
+}
---
+++ Source/ALSV4_CPP/Public/Settings/ALSSettingsLocal.h
@@ -0,0 +1,109 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "Input/ALSMappableConfigPair.h"
+#include "ALSSettingsLocal.generated.h"
+
+class UPlayerMappableInputConfig;
+class UALSLocalPlayer;
+
+/**
+ * UALSSettingsLocal
+ */
+UCLASS()
+class ALSV4_CPP_API UALSSettingsLocal : public UGameUserSettings
+{
+	GENERATED_BODY()
+
+public:
+	UALSSettingsLocal();
+
+	static UALSSettingsLocal* Get();
+
+	//~UObject interface
+	virtual void BeginDestroy() override;
+	//~End of UObject interface
+
+	// INPUT CONFIGS
+public:
+	// Sets the controller representation to use, a single platform might support multiple kinds of controllers.  For
+	// example, Win64 games could be played with both an XBox or Playstation controller.
+	UFUNCTION()
+	void SetControllerPlatform(const FName InControllerPlatform);
+	UFUNCTION()
+	FName GetControllerPlatform() const;
+
+	DECLARE_EVENT_OneParam(UALSSettingsLocal, FInputConfigDelegate, const FLoadedMappableConfigPair& /*Config*/);
+
+	/** Delegate called when a new input config has been registered */
+	FInputConfigDelegate OnInputConfigRegistered;
+
+	/** Delegate called when a registered input config has been activated */
+	FInputConfigDelegate OnInputConfigActivated;
+	
+	/** Delegate called when a registered input config has been deactivate */
+	FInputConfigDelegate OnInputConfigDeactivated;
+	
+	/** Register the given input config with the settings to make it available to the player. */
+	void RegisterInputConfig(ECommonInputType Type, const UPlayerMappableInputConfig* NewConfig, const bool bIsActive);
+	
+	/** Unregister the given input config. Returns the number of configs removed. */
+	int32 UnregisterInputConfig(const UPlayerMappableInputConfig* ConfigToRemove);
+
+	/** Set a registered input config as active */
+	void ActivateInputConfig(const UPlayerMappableInputConfig* Config);
+
+	/** Deactivate a registered config */
+	void DeactivateInputConfig(const UPlayerMappableInputConfig* Config);
+
+	/** Get an input config with a certain name. If the config doesn't exist then nullptr will be returned. */
+	UFUNCTION(BlueprintCallable)
+	const UPlayerMappableInputConfig* GetInputConfigByName(FName ConfigName) const;
+
+	/** Get all currently registered input configs */
+	const TArray<FLoadedMappableConfigPair>& GetAllRegisteredInputConfigs() const { return RegisteredInputConfigs; }
+
+	/**
+	 * Get all registered input configs that match the input type.
+	 * 
+	 * @param Type		The type of config to get, ECommonInputType::Count will include all configs.
+	 * @param OutArray	Array to be populated with the current registered input configs that match the type
+	 */
+	void GetRegisteredInputConfigsOfType(ECommonInputType Type, OUT TArray<FLoadedMappableConfigPair>& OutArray) const;
+	
+	/**
+	 * Maps the given keyboard setting to the 
+	 * 
+	 * @param MappingName	The name of the FPlayerMappableKeyOptions that you would like to change
+	 * @param NewKey		The new key to bind this option to
+	 */
+	void AddOrUpdateCustomKeyboardBindings(const FName MappingName, const FKey NewKey, UALSLocalPlayer* LocalPlayer);
+
+	const TMap<FName, FKey>& GetCustomPlayerInputConfig() const { return CustomKeyboardConfig; }
+
+private:
+	UPROPERTY(Config)
+	FName ControllerPlatform;
+
+	UPROPERTY(Config)
+	FName ControllerPreset = TEXT("Default");
+
+	/** The name of the current input config that the user has selected. */
+	UPROPERTY(Config)
+	FName InputConfigName = TEXT("Default");
+	
+	/**
+	 * Array of currently registered input configs. This is populated by game feature plugins
+	 * 
+	 * @see UGameFeatureAction_AddInputConfig
+	 */
+	UPROPERTY(VisibleAnywhere)
+	TArray<FLoadedMappableConfigPair> RegisteredInputConfigs;
+	
+	/** Array of custom key mappings that have been set by the player. Empty by default. */
+	UPROPERTY(Config)
+	TMap<FName, FKey> CustomKeyboardConfig;
+};
