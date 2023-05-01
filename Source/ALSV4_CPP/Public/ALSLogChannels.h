006e784da232d0e98c47639d8600d5095e0dbf14
Helcio Laurentino do Carmo Junior <helcio@carmo.in>
2023-05-01 18:26:52 -0300

    feat: Add LogChannel class

---
+++ Source/ALSV4_CPP/Public/ALSLogChannels.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Logging/LogMacros.h"
+
+ALSV4_CPP_API DECLARE_LOG_CATEGORY_EXTERN(LogALS, Log, All);
+ALSV4_CPP_API DECLARE_LOG_CATEGORY_EXTERN(LogALSAbilitySystem, Log, All);
+ALSV4_CPP_API DECLARE_LOG_CATEGORY_EXTERN(LogALSExperience, Log, All);
+
+ALSV4_CPP_API FString GetClientServerContextString(UObject* ContextObject = nullptr);
