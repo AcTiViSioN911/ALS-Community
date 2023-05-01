128f5a9f5af3928fce8975a17a99342de048a5fd
Helcio Laurentino do Carmo Junior <helcio@carmo.in>
2023-05-01 18:31:19 -0300

    feat(physic): Add Physic class

---
+++ Source/ALSV4_CPP/Private/Physics/PhysicalMaterialWithTags.cpp
@@ -0,0 +1,8 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Physics/PhysicalMaterialWithTags.h"
+
+UPhysicalMaterialWithTags::UPhysicalMaterialWithTags(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
