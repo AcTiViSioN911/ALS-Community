f9c458c438058a4698d6e733936346d0cacd76bb
Helcio Laurentino do Carmo Junior <helcio@carmo.in>
2023-05-01 18:27:49 -0300

    feat: Add Camera classes

---
+++ Source/ALSV4_CPP/Public/Camera/ALSCameraComponent.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "ALSCameraComponent.generated.h"
+
+class UALSCameraMode;
+
+DECLARE_DELEGATE_RetVal(TSubclassOf<UALSCameraMode>, FALSCameraModeDelegate);
+
+/**
+ * 
+ */
+UCLASS()
+class ALSV4_CPP_API UALSCameraComponent : public UCameraComponent
+{
+	GENERATED_BODY()
+
+public:
+	UALSCameraComponent(const FObjectInitializer& ObjectInitializer);
+
+	// Returns the camera component if one exists on the specified actor.
+	UFUNCTION(BlueprintPure, Category = "ALS|Camera")
+	static UALSCameraComponent* FindCameraComponent(const AActor* Actor) { return (Actor ? Actor->FindComponentByClass<UALSCameraComponent>() : nullptr); }
+	
+	// Delegate used to query for the best camera mode.
+	FALSCameraModeDelegate DetermineCameraModeDelegate;
+};
---
+++ Source/ALSV4_CPP/Public/Camera/ALSCameraMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ALSCameraMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALSV4_CPP_API UALSCameraMode : public UObject
+{
+	GENERATED_BODY()
+};
