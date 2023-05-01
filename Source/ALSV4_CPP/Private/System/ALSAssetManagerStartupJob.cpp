09f3382f85282cc08d12c35dbb8b267c30dbeb8f
Helcio Laurentino do Carmo Junior <helcio@carmo.in>
2023-05-01 18:30:41 -0300

    feat(system): Add system classes

---
+++ Source/ALSV4_CPP/Private/System/ALSAssetManagerStartupJob.cpp
@@ -0,0 +1,29 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "System/ALSAssetManagerStartupJob.h"
+
+#include "HAL/Platform.h"
+#include "Logging/LogCategory.h"
+#include "Logging/LogMacros.h"
+#include "ALSLogChannels.h"
+#include "Trace/Detail/Channel.h"
+
+TSharedPtr<FStreamableHandle> FALSAssetManagerStartupJob::DoJob() const
+{
+	const double JobStartTime = FPlatformTime::Seconds();
+
+	TSharedPtr<FStreamableHandle> Handle;
+	UE_LOG(LogALS, Display, TEXT("Startup job \"%s\" starting"), *JobName);
+	JobFunc(*this, Handle);
+
+	if (Handle.IsValid())
+	{
+		Handle->BindUpdateDelegate(FStreamableUpdateDelegate::CreateRaw(this, &FALSAssetManagerStartupJob::UpdateSubstepProgressFromStreamable));
+		Handle->WaitUntilComplete(0.0f, false);
+		Handle->BindUpdateDelegate(FStreamableUpdateDelegate());
+	}
+
+	UE_LOG(LogALS, Display, TEXT("Startup job \"%s\" took %.2f seconds to complete"), *JobName, FPlatformTime::Seconds() - JobStartTime);
+
+	return Handle;
+}
---
+++ Source/ALSV4_CPP/Private/System/ALSGameData.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "System/ALSGameData.h"
+#include "System/ALSAssetManager.h"
+
+UALSGameData::UALSGameData()
+{
+}
+
+const UALSGameData& UALSGameData::UALSGameData::Get()
+{
+	return UALSAssetManager::Get().GetGameData();
+}
